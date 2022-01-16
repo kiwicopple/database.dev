aaa92254e73f69b7c521bffa4108cf82b60052c7
Copple <10214025+kiwicopple@users.noreply.github.com>
2022-01-16 21:39:54 +0100

    move config to an env file

--- website/pages/lib/supabaseClient.ts
+++ website/pages/lib/supabaseClient.ts
@@ -2,9 +2,8 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 let supabaseSingleton: null | SupabaseClient = null;
 
-const SUPABASE_URL = "http://localhost:54321";
-const SUPABASE_KEY_ANON =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiJ9.ZopqoUt20nEV9cklpv9e3yw3PVyZLmKs5qLD6nGL1SI";
+const SUPABASE_URL = process.env.SUPABASE_URL || "http://localhost:54321";
+const SUPABASE_KEY_ANON = process.env.SUPABASE_KEY_ANON || "";
 
 if (supabaseSingleton === null) {
   supabaseSingleton = createClient(SUPABASE_URL, SUPABASE_KEY_ANON);
